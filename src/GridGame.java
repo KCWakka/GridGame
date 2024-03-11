b428b51e18f54140f6a2553185beec211002e2a8
BT_2N2_06 <sample@gmail.com>
2024-03-11 14:03:54 -0400

    done

--- src/GridGame.java
+++ src/GridGame.java
@@ -26,9 +26,31 @@ public class GridGame {
     // each with a point value that you decide, and place them throughout the board
     private void setupBoard() {
         // WRITE THIS METHOD
-
-
-
+        board = new Space[8][8];
+        for (int row = 0; row < board.length; row++ ) {
+            Boolean placeTreasure = true;
+            for (int col = 0; col < board[row].length; col++) {
+                int random2 = (int) (Math.random() * 8) + 1;
+                if (row == 7 && col == 0) {
+                    board[row][col] = player;
+                } else {
+                    if (row == 0 && col == 7) {
+                        Goal goal = new Goal("X");
+                        board[row][col] = goal;
+                    } else {
+                        if (placeTreasure && col != 0 && (col + 1) % random2 == 0) {
+                            int point = (int) (Math.random() * 61) + 10;
+                            Treasure treasure = new Treasure("M", point);
+                            board[row][col] = treasure;
+                            placeTreasure = false;
+                        } else {
+                            Space space = new Space("_");
+                            board[row][col] = space;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /* prints the 2D array board, showing the symbol for each Space, e.g.
@@ -43,13 +65,16 @@ public class GridGame {
      */
     private void printBoard() {
         // WRITE THIS METHOD
-
-
+        for (Space[] row : board) {
+            for (Space col : row) {
+                System.out.print(col.getSymbol());
+            }
+            System.out.println();
+        }
     }
 
     // plays the game;
     private void play() {
-
         // WRITE THIS METHOD
         // main game loop:
         // while the player has not yet reached the goal, print the board (complete can call helper method below)
@@ -59,8 +84,84 @@ public class GridGame {
         // if player moves to a position occupied by a Treasure, add its point value to the players score,
         // and replace that element with a Space object (with "_" symbol).
         // if the player reaches the goal, end the game and print their final score and the number of moves it took
+        while (board[7][7] != player) {
+            printBoard();
+            System.out.print("Enter W, A, S, D: ");
+            String choice = scanner.nextLine();
+            if (processChoice(choice) != 0) {
+                if (choice.equals("d")) {
+                    if (findPlayer()[1] + 1 < 8) {
+                        Space space = new Space("_");
+                        int row = findPlayer()[0];
+                        int col = findPlayer()[1];
+                        board[row][col] = space;
+                        board[row][col + 1] = player;
+                    } else {
+                        System.out.println("You will go out of bound!");
+                    }
+                }
+                if (choice.equals("a")) {
+                    if (findPlayer()[1] - 1 > -1) {
+                        Space space = new Space("_");
+                        int row = findPlayer()[0];
+                        int col = findPlayer()[1];
+                        board[row][col] = space;
+                        board[row][col - 1] = player;
+                    } else {
+                        System.out.println("You will go out of bound!");
+                    }
+                }
+                if (choice.equals("w")) {
+                    if (findPlayer()[0] - 1 > -1) {
+                        Space space = new Space("_");
+                        int row = findPlayer()[0];
+                        int col = findPlayer()[1];
+                        board[row][col] = space;
+                        board[row - 1][col] = player;
+                    } else {
+                        System.out.println("You will go out of bound!");
+                    }
+                }
+                if (choice.equals("s")) {
+                    if (findPlayer()[0] + 1 < 8) {
+                        Space space = new Space("_");
+                        int row = findPlayer()[0];
+                        int col = findPlayer()[1];
+                        board[row][col] = space;
+                        board[row + 1][col] = player;
+                    } else {
+                        System.out.println("You will go out of bound!");
+                    }
+                }
+            } else {
+                System.out.println("INVALID DIRECTION!");
+            }
+        }
+    }
 
+    private int processChoice(String choice) {
+        choice.toLowerCase();
+        if (choice.equals("d") || choice.equals("w")) {
+            return 1;
+        } else if (choice.equals("a") || choice.equals("s")) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 
-
+    private int[] findPlayer() {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] instanceof Player) {
+                    int[] array = new int[2];
+                    array[0] = row;
+                    array[1] = col;
+                    return array;
+                }
+            }
+        }
+        int[] temp = new int[1];
+        return temp;
     }
 }
